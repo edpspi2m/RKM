2b10c7662d35bb8a9e9d2d43d3cedbf0f9067f4d
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-04 13:59:59 +0700

    Update api_constant.dart

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -1,12 +1,10 @@
 class ApiConstant {
-  // Base URL API Production
   static const String baseUrl = "https://api.isreport.my.id/absen";
 
-  // Endpoints
   static const String login = "/login.php";
   static const String timestamp = "/timestamp.php";
   static const String submitKunjungan = "/rkm.php";
+  static const String promo = "/promo.php";
 
-  // Timeout
   static const Duration timeout = Duration(seconds: 15);
 }
