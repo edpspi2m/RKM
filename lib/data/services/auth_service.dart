20ddc66a1d85f4ef1a63ca712f414fa4b865611e
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-04 09:46:54 +0700

    Update auth_service.dart

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -4,20 +4,17 @@ import '../models/user_model.dart';
 
 class AuthService {
   final ApiClient _apiClient;
-
   AuthService(this._apiClient);
 
   Future<UserModel> login({required String email, required String password}) async {
     final response = await _apiClient.post(
       ApiConstant.login,
-      body: {'email': email, 'password': password},
+      body: {'username': email, 'password': password},
     );
-
     final data = response['data'] as Map<String, dynamic>?;
     if (data == null) {
       throw Exception('Format respons login tidak valid.');
     }
-
     return UserModel.fromJson(data);
   }
 }
