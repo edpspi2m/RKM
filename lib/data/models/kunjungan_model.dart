cc2cc868be5df6264e5480f0f87569fafc120f24
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-06 14:35:20 +0700

    Update kunjungan_model.dart

--- lib/data/models/kunjungan_model.dart
+++ lib/data/models/kunjungan_model.dart
@@ -5,15 +5,18 @@ class KunjunganModel {
   final String catatan;
   final GpsLocationModel lokasi;
   final String fotoPath;
+  final String username;
 
   const KunjunganModel({
     required this.namaToko,
     required this.catatan,
     required this.lokasi,
     required this.fotoPath,
+    required this.username,
   });
 
   Map<String, String> toFields() => {
+        'username': username,
         'nama_toko': namaToko,
         'catatan': catatan,
         'latitude': lokasi.latitude.toString(),
