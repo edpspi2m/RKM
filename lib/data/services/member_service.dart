9a60eb346934d16e3727a73a130890af35d36d76
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-07 15:05:58 +0700

    Update member_service.dart

--- lib/data/services/member_service.dart
+++ lib/data/services/member_service.dart
@@ -5,11 +5,8 @@ class MemberService {
   final ApiClient _apiClient;
   MemberService(this._apiClient);
 
-  Future<List<MemberModel>> fetchMembers(String username) async {
-    final response = await _apiClient.post(
-      '/members_list.php',
-      body: {'username': username},
-    );
+  Future<List<MemberModel>> fetchMembers(String userId) async {
+    final response = await _apiClient.post('/members_list.php', body: {'user_id': userId});
     final list = response['data'] as List<dynamic>? ?? [];
     return list.map((e) => MemberModel.fromJson(e as Map<String, dynamic>)).toList();
   }
