d7659452e4e40634fbea1ea0b8da52d9bde3ad7d
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-13 21:27:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'app/app.dart';
+import 'core/background/background_location_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID', null);
+  await BackgroundLocationHandler.initialize();
   runApp(const RkmApp());
 }
