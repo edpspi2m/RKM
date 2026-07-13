c1f55af0eb0e58a9c9bac0c3a376ec2db9b65639
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-13 23:05:29 +0700

    Update api_constant.dart

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -5,6 +5,10 @@ class ApiConstant {
   static const String timestamp = "/timestamp.php";
   static const String submitKunjungan = "/rkm.php";
   static const String promo = "/promo.php";
+  
+  // Tambahan endpoint untuk OTP (Ubah nama file .php jika berbeda di backend)
+  static const String otpRequest = "/otp_request.php"; 
+  static const String otpVerify = "/otp_verify.php";
 
   static const Duration timeout = Duration(seconds: 15);
 }
