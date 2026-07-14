c70b95dbba76f57661b17912522c2cf5d6a3d279
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-14 16:47:53 +0700

    Update route_tracking_provider.dart

--- lib/providers/route_tracking_provider.dart
+++ lib/providers/route_tracking_provider.dart
@@ -10,10 +10,19 @@ const _bufferKey = 'route_point_buffer';
 class RouteTrackingProvider extends ChangeNotifier {
   final RouteTrackingService _service;
 
-  RouteTrackingProvider(this._service);
+  RouteTrackingProvider(this._service) {
+    BackgroundLocationHandler.onFakeGpsDetected.listen((event) {
+      _isTracking = false;
+      _fakeGpsDetected = true;
+      notifyListeners();
+    });
+  }
 
   bool _isTracking = false;
+  bool _fakeGpsDetected = false;
+
   bool get isTracking => _isTracking;
+  bool get fakeGpsDetected => _fakeGpsDetected;
 
   Future<void> checkInitialState() async {
     _isTracking = await BackgroundLocationHandler.isRunning();
@@ -21,6 +30,7 @@ class RouteTrackingProvider extends ChangeNotifier {
   }
 
   Future<void> startTracking(String userId) async {
+    _fakeGpsDetected = false;
     await BackgroundLocationHandler.start(userId);
     _isTracking = true;
     notifyListeners();
@@ -40,6 +50,11 @@ class RouteTrackingProvider extends ChangeNotifier {
     }
   }
 
+  void clearFakeGpsFlag() {
+    _fakeGpsDetected = false;
+    notifyListeners();
+  }
+
   Future<void> uploadPendingPoints(String userId) async {
     final prefs = await SharedPreferences.getInstance();
     final key = '${_bufferKey}_$userId';
@@ -52,8 +67,6 @@ class RouteTrackingProvider extends ChangeNotifier {
           .toList();
       await _service.submitPoints(userId: userId, points: points);
       await prefs.remove(key);
-    } catch (_) {
-      // Gagal upload → buffer tetap, dicoba lagi next launch
-    }
+    } catch (_) {}
   }
 }
