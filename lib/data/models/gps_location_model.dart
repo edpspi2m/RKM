10696d7a0dabc42f8058584d077ded4d33b100e7
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-16 10:22:22 +0700

    Update gps_location_model.dart

--- lib/data/models/gps_location_model.dart
+++ lib/data/models/gps_location_model.dart
@@ -3,20 +3,13 @@ class GpsLocationModel {
   final double longitude;
   final String address;
   final DateTime capturedAt;
+  final double? accuracy;
 
   const GpsLocationModel({
     required this.latitude,
     required this.longitude,
     required this.address,
     required this.capturedAt,
+    this.accuracy,
   });
-
-  String get coordinateText => '${latitude.toStringAsFixed(6)}, ${longitude.toStringAsFixed(6)}';
-
-  Map<String, dynamic> toJson() => {
-        'latitude': latitude,
-        'longitude': longitude,
-        'address': address,
-        'captured_at': capturedAt.toIso8601String(),
-      };
 }
