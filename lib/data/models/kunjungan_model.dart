a3ea0d7c5162d29c7ce257b5c6e14a3c048409f6
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-21 15:13:10 +0700

    Update kunjungan_model.dart

--- lib/data/models/kunjungan_model.dart
+++ lib/data/models/kunjungan_model.dart
@@ -9,16 +9,19 @@ class KunjunganModel {
   final String kelurahan;
   final String kecamatan;
   final String kota;
+  final bool fromMemberList;
 
   const KunjunganModel({
     required this.userId, required this.member, required this.catatan, required this.lokasi,
     this.statusKunjungan = 'berhasil', this.kelurahan = '', this.kecamatan = '', this.kota = '',
+    this.fromMemberList = false,
   });
 
   Map<String, String> toFields() => {
     'user_id': userId, 'member': member, 'catatan': catatan,
     'latitude': lokasi.latitude.toString(), 'longitude': lokasi.longitude.toString(),
     'timestamp': lokasi.capturedAt.toIso8601String(), 'status_kunjungan': statusKunjungan,
+    'from_member_list': fromMemberList ? '1' : '0',
     if (kelurahan.isNotEmpty) 'kelurahan': kelurahan,
     if (kecamatan.isNotEmpty) 'kecamatan': kecamatan,
     if (kota.isNotEmpty) 'kota': kota,
