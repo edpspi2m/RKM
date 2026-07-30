03d1da000156c00f6fff64fa39f915d0feb471bb
edpspi2m <wfhedpspi2m@gmail.com>
2026-07-30 09:32:52 +0700

    Update app_colors.dart

--- lib/app/theme/app_colors.dart
+++ lib/app/theme/app_colors.dart
@@ -5,10 +5,10 @@ class AppColors {
 
   static const Color primary = Color(0xFF0D47A1);
   static const Color primaryDark = Color(0xFF08306B);
-  static const Color primaryLight = Color(0xFF5472D3);
+  static const Color primaryLight = Color(0xFFEAF1FC);
 
-  static const Color action = Color(0xFF00A876); // hijau segar, lebih hidup dari sebelumnya
-  static const Color actionDark = Color(0xFF00754F);
+  static const Color action = Color(0xFF00A876);
+  static const Color actionLight = Color(0xFFE3F7EF);
 
   static const Color background = Color(0xFFF4F7FA);
   static const Color surface = Colors.white;
@@ -20,16 +20,4 @@ class AppColors {
 
   static const Color error = Color(0xFFE5484D);
   static const Color warning = Color(0xFFF5A623);
-
-  static const LinearGradient primaryGradient = LinearGradient(
-    colors: [primary, primaryLight],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-  );
-
-  static const LinearGradient actionGradient = LinearGradient(
-    colors: [action, Color(0xFF00C896)],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-  );
 }
