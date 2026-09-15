18714245a306c815a5ae6d423b80503beb56a282
edpspi2m <wfhedpspi2m@gmail.com>
2026-09-15 16:01:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'app/app.dart';
+import 'core/background/background_location_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID', null);
+
+  // Init background service
+  // Biar HP restart pun service auto-nyala (autoStartOnBoot: true)
+  await BackgroundLocationHandler.initialize();
+
   runApp(const RkmApp());
 }
